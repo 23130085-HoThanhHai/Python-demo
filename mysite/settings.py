a1b5ffa82ad1f6397e30c1d8945a5b69222aba61
Ho Thanh Hai <23130085@st.hcmuaf.edu.vn>
2025-11-03 11:56:59 +0700

    update và lưu y là cài django phiên ban thap hơn

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_quay',  # nhớ đổi thành tên DB thật
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
